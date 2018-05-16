4da068631fea510dd5648e1f8b31e219b7164680
Bryce Harrington <bryce@bryceharrington.org>
2018-05-16 14:00:36 -0700

    fixedpt: Begin work on experiment to test fp vs floating point operations
    Signed-off-by: Bryce Harrington <bryce@bryceharrington.org>

--- fixedpt/README.md
+++ fixedpt/README.md
@@ -1,14 +1,20 @@
 Fp-Perf-Exp
 ------------------------------------------------------------------------
 
-This is a .... that aims to:
+This program analyzes performance of fixed point data types against
+double.  It uses Ivan Voras' fixedpt library, which provides 64-bit data
+types on processors that can support it.
 
- * ...
- * ...
+These experiments are intended to inform Cairo Vulkan development.
+Cairo uses fixed point internally, but it's unclear if this is strictly
+just for core algorithmic requirements or if it has an performance
+impact as well.  (Fixed point is sometimes used where the hardware does
+not support floating point math, but Cairo uses plenty of floating point
+throughout already.)
 
 Fp-Perf-Exp is provided under the gpl-2.0 license.
 
-For more information, see ...
+For more information, see the Cairo mailing list.
 
 ### Building
 
@@ -23,6 +29,6 @@ Building Fp-Perf-Exp is fairly straightforward:
 
 ### Dependencies
 
- * ...
- * ...
+ * None
+
 
--- fixedpt/src/main.c
+++ fixedpt/src/main.c
@@ -1,21 +1,93 @@
 /*
- * 
+ * fixed-pt-exp - Performance experiments
  *
  * Copyright © 2018 Bryce Harrington
  *
  * Released under gpl-2.0, read the file 'COPYING' for more information.
- *
- * Contributors:
  */
 
-/*
- * This file defines ...
- * It is based on ...
- * It is used by ...
- */
-
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+
+#define FIXEDPT_BITS (64)
+#include "fixedpt/fixedptc.h"
+
+#define MAX_OPERATIONS (1024)
+
+// TODO: Define these for all data types
+void add() {}
+void subtract() {}
+void multiply() {}
+void divide() {}
+void square() {}
+void square_root() {}
+
+typedef enum _operation {
+  OP_SQRT = -3,
+  OP_DIV = -2,
+  OP_SUB = -1,
+
+  OP_NONE = 0,
+
+  OP_ADD = 1,
+  OP_MUL = 2,
+  OP_SQR = 3,
+
+  OP_LAST = 4
+} operation_t;
+
+/* Returns an integer in the range [0, n).
+ *
+ * Uses rand(), and so is affected-by/affects the same seed.
+ *
+ * https://stackoverflow.com/questions/822323/how-to-generate-a-random-number-in-c
+ * License: Creative Commons Attribution-Share Alike
+ */
+int randint(int n) {
+  if ((n - 1) == RAND_MAX) {
+    return rand();
+  } else {
+    // Supporting larger values for n would requires an even more
+    // elaborate implementation that combines multiple calls to rand()
+    assert(n <= RAND_MAX);
+
+    // Chop off all of the values that would cause skew...
+    int end = RAND_MAX / n; // truncate skew
+    assert(end > 0);
+    end *= n;
+
+    // ... and ignore results from rand() that fall above that limit.
+    // (Worst case the loop condition should succeed 50% of the time,
+    // so we can expect to bail out of this loop pretty quickly.)
+    int r;
+    while ((r = rand()) >= end);
+
+    return r % n;
+  }
+}
 
 int main() {
-  printf("Hello world\n");
+  // Generate a random sequence of operations, and their opposites
+  operation_t operations[MAX_OPERATIONS];
+  int num_operations = 10;
+
+  srand(time(0));
+
+  assert(num_operations <= MAX_OPERATIONS);
+  for (int i=0; i<num_operations; i++) {
+    operations[i] = randint(OP_LAST);
+  }
+
+  // Do sets of ops in short sequences
+
+  // Run for several numerical ranges - small, medium, large, huge
+
+  // Compare performance
+  // Note where range errors occur
+  for (int i=0; i<num_operations; i++) {
+    printf("Hello operation %d\n", operations[i]);
+  }
 }
